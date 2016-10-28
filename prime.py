6d4019c4dbd21ba209bc8bf7ca79ce92be475298
Wahu <kinuthiaj.wahu@gmail.com>
2016-10-28 18:33:50 +0300

    Add method is prime

--- prime.py
+++ prime.py
@@ -1,1 +1,14 @@
-def is_prime 
+def is_prime(number):
+	if number < 2:
+		return False
+	elif number == 2 or number == 3:
+		return True
+	else:
+		if number % 2 == 0: #removing the even numbers
+			return False
+def prime_num(primes):
+	primes = list()
+	for i in range (3, number):
+		if nu
+			return primes
+print (prime_num(30))
